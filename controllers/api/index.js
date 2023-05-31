37691ad513cdc519c732c2ec81df6a05cf8d8362
Rhonda Stokes <rhonda@aerostokes.com>
2023-05-31 14:14:42 -0700

    add dmController

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const userRoutes = require("./userController");
 router.use("/users", userRoutes);
 
+const dmRoutes = require("./dmController");
+router.use("/dms", dmRoutes);
+
 module.exports = router;
